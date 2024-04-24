522f7bcb219d9657b8c12e5b24e33af8fc3e09b5
madgenuis <132010700+madgenuis@users.noreply.github.com>
2024-04-24 18:29:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import base64
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("stadium.jpg")
+# data:image/png;base64,{img}
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img}");
+width: 100%;
+height: 100%;
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-size: cover;
+color: yellow; /* Set text color to yellow */
+}}
+
+/* Set text color for specific elements */
+h1, h2, h3, h4, h5, h6, p, span, label, button, input, select, textarea {{
+color: yellow;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+teams = ['Sunrisers Hyderabad', 'Mumbai Indians', 'Royal Challengers Bangalore', 'Kolkata Knight Riders', 'Kings XI Punjab', 'Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals']
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi', 'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth', 'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley', 'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala', 'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi', 'Sharjah', 'Mohali', 'Bengaluru']
+
+pipe = pickle.load(open(r'C:\Users\vagee\OneDrive\Desktop\ipl-proj\pipe.pkl', 'rb'))
+st.title('IPL Predictor')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team', sorted(teams))
+with col2:
+    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
+
+selected_city = st.selectbox('Select host city', sorted(cities))
+
+target = st.number_input('Target')
+
+col3, col4, col5 = st.columns(3)
+
+with col3:
+    score = st.number_input('Score')
+with col4:
+    overs = st.number_input('Overs completed')
+with col5:
+    wickets = st.number_input('Wickets out')
+
+if st.button('Predict Probability'):
+    if score > target:
+        st.write(f"{batting_team} has already won!")
+    else:
+        runs_left = target - score
+        balls_left = 120 - (overs * 6)
+        wickets_left = 10 - wickets
+        crr = score / overs
+        rrr = (runs_left * 6) / balls_left
+
+        input_df = pd.DataFrame({
+            'batting_team': [batting_team],
+            'bowling_team': [bowling_team],
+            'city': [selected_city],
+            'runs_left': [runs_left],
+            'balls_left': [balls_left],
+            'wickets': [wickets_left],  # Corrected column name
+            'total_runs_x': [target],
+            'crr': [crr],
+            'rrr': [rrr]
+        })
+
+
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
+        st.header(batting_team + "- " + str(round(win * 100)) + "%")
+        st.header(bowling_team + "- " + str(round(loss * 100)) + "%")
